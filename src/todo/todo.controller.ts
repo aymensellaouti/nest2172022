c7dbb15c5a93169d4d04fd471b93b42e34f22957
aymen2782 <aymen.sellaouti@gmail.com>
2022-07-22 08:45:42 +0100

    update delete getById Todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { Todo } from './Model/todo.model';
 
@@ -18,5 +28,36 @@ export class TodoController {
     newTodo.description = description;
     this.todos.push(newTodo);
     return newTodo;
+  }
+  @Get(':id')
+  getTodoById(@Param('id') id: string): Todo {
+    return this.findTodoById(id);
+  }
+  @Delete(':id')
+  deleteTodoById(@Param('id') id: string): { count: number } {
+    const todo = this.findTodoById(id);
+    this.todos = this.todos.filter((actualTodo) => actualTodo.id != id);
+    return { count: 1 };
+  }
+
+  @Put(':id')
+  updateTodoById(
+    @Param('id') id: string,
+    @Body() updatedTodo: Partial<Todo>,
+  ): Todo {
+    const todo = this.findTodoById(id);
+    const newTodo = { ...todo, ...updatedTodo };
+    todo.description = updatedTodo.description ?? todo.description;
+    todo.name = updatedTodo.name ?? todo.name;
+    todo.status = updatedTodo.status ?? todo.status;
+    return todo;
+  }
+
+  private findTodoById(id: string): Todo {
+    const todo = this.todos.find((todo) => todo.id == id);
+    if (!todo) {
+      throw new NotFoundException(`Todo innexistant`);
+    }
+    return todo;
   }
 }
